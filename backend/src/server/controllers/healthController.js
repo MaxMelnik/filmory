9d8a3fd86b78a16afb81c5819516b2fe71c43997
Maks Melnyk <maks.melnik1611@gmail.com>
2025-11-07 20:52:05 +0200

    remove passed healthz log

--- backend/src/server/controllers/healthController.js
+++ backend/src/server/controllers/healthController.js
@@ -6,8 +6,7 @@ export const healthController = {
             const data = await HealthService.getFullHealth();
             const allOk = Object.values(data.services).every(s => s.status === 'ok');
             // res.status(allOk ? 200 : 503).json(data);
-            console.log(allOk);
-            if (!allOk) console.log(data);
+            if (!allOk) console.log('Healthcheck:', data);
             res.json(data);
         } catch (err) {
             res.status(500).json({status: 'error', error: err.message});

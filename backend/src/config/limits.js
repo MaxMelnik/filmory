494cd84a70dcc42452e281edb030c6a4dc47be48
Maks Melnyk <maks.melnik1611@gmail.com>
2025-12-10 17:48:00 +0200

    return old limits

--- backend/src/config/limits.js
+++ backend/src/config/limits.js
@@ -1,3 +1,3 @@
-export const FREE_DAILY_LIMIT = 1;
-export const PLUS_DAILY_LIMIT = 1;
+export const FREE_DAILY_LIMIT = 5;
+export const PLUS_DAILY_LIMIT = 111;
 export const MIN_REQUEST_INTERVAL_MS = 3000;

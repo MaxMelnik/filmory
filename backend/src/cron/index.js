52e24c4501d321b851d5d62ca017cf26a0f5e631
Maks Melnyk <maks.melnik1611@gmail.com>
2025-12-26 19:27:31 +0200

    update cron time

--- backend/src/cron/index.js
+++ backend/src/cron/index.js
@@ -16,7 +16,7 @@ export default function() {
     });
 
     if (process.env.ENVIRONMENT === 'PROD') {
-        const postDailyRecommendationCronRule  = (process.env.ENVIRONMENT === 'DEV') ? '* * * * *' : '0 17 25 * *';
+        const postDailyRecommendationCronRule  = (process.env.ENVIRONMENT === 'DEV') ? '* * * * *' : '30 17 * * *';
         logger.info(`postDailyRecommendationCronRule: ${postDailyRecommendationCronRule}`);
         cron.schedule(postDailyRecommendationCronRule, async () => {
             await postDailyRecommendation(bot);
